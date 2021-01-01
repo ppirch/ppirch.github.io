7c311269f46ad6a66ebba35bdd6d99d17258e5c0
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-01 21:23:33 +0700

    Edit Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,18 +4,21 @@ import { withStyles } from "@material-ui/core/styles"
 
 const styles = (theme) => ({
   root: {
-    padding: 0,
-    height: "100vh",
     backgroundColor: "#d4f0fc",
   },
+  gridLayout: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
 })
+
 class Home extends React.Component {
   render() {
     const { classes } = this.props
 
     return (
       <Container maxWidth={false} className={classes.root}>
-        <Grid container>
+        <Grid container className={classes.gridLayout}>
           <Grid item xs={12} sm={12}>
             <h1>Hello World</h1>
           </Grid>
