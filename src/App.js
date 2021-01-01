84873ad61b62bbdc573099bc58018c4b710719ff
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-01 16:40:47 +0700

    Restructure project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react"
+import { Switch, Route } from "react-router-dom"
+import Home from "./pages"
+const About = () => <h1>About</h1>
+const Post = () => <h1>Post</h1>
+const Project = () => <h1>Project</h1>
+const NotFoundPage = () => (
+  <h1>
+    404 <br />
+    Page Not Found
+  </h1>
+)
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/posts" component={Post} />
+          <Route path="/projects" component={Project} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
