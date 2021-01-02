1815fdee67697487325ba17968cb2a176245ae95
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-02 18:44:11 +0700

    Remove unused import

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,7 +20,6 @@ import ListItemText from "@material-ui/core/ListItemText"
 import ListItemAvatar from "@material-ui/core/ListItemAvatar"
 import Avatar from "@material-ui/core/Avatar"
 import { Call, Mail, Place } from "@material-ui/icons"
-import BeachAccessIcon from "@material-ui/icons/BeachAccess"
 
 import PropTypes from "prop-types"
 
