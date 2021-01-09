f2f135282bfd31fcf219d05d057aba65a73eece2
Pakin Pirch <pakin.pirch@gmail.com>
2021-01-10 01:10:06 +0700

    Change path and add .env to .gitignore

--- src/api/ppirch-ai-api.js
+++ src/api/ppirch-ai-api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const apiUrl = process.env.REACT_APP_PPIRCH_AI_API || "http://localhost:8000"
+const apiUrl =
+  process.env.REACT_APP_PPIRCH_AI_API || "https://ppirch-ai-api.herokuapp.com"
 
 export const getLocationTag = async (text) => {
   const queryURL = `${apiUrl}/nlp/location-tag`
