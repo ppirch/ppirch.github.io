c0ab187df9fd8213c602870c876ea8cca75e6b4d
ppirch <pakin.pirch@gmail.com>
2024-10-27 01:52:20 +0800

    fix: change adapter settings

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,12 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'app.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: 'app.html',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : ''
